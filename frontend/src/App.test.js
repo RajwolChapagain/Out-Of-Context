3a4f4000731382a33ef551342e9bb73725023df6
hiraniziyan <141653186+hiraniziyan@users.noreply.github.com>
2026-02-04 13:22:49 -0500

    Front end

---
+++ frontend/src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from './Chat'; // This tells React to find your Chat.js file
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
